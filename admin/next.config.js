0cc9c400916e351da808c82a959812ec0ed56d45
tky-ymnk-1217 <takaya.yamanaka.1217@gmail.com>
2026-05-12 14:11:03 +0900

    configの修正

--- admin/next.config.js
+++ admin/next.config.js
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
+
   env: {
     API_URL: process.env.API_URL || 'http://localhost:8000/api',
   },
+
   output: 'export',
+
+  ...(isProd && {
+    basePath: '/admin',
+    assetPrefix: '/admin/',
+  }),
 }
 
 module.exports = nextConfig
